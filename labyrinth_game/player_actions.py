13c22c75d6a3f59714e3b7cf7143807953f78f27
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-09-24 19:45:33 +0300

    add move_player

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,3 +1,5 @@
+import labyrinth_game.utils
+
 def show_inventory(game_state):
   inventory = game_state['player_inventory']
   if (inventory):
@@ -13,4 +15,11 @@ def get_input(prompt="> "):
     print("\nВыход из игры.")
     return "quit"
 
-
+def move_player(game_state, direction):
+  exit = game_state['exits'].get(direction)
+  if (exit):
+    game_state['current_room'] = exit
+    game_state['steps_taken'] += 1
+    labyrinth_game.utils.describe_current_room(game_state)
+  else:
+    print("Нельзя пойти в этом направлении.")  

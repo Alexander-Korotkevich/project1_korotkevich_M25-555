d0ca127a1e37fbd17104140c465459982e8b8f64
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-09-24 20:26:35 +0300

    rename var

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -3,16 +3,16 @@ from labyrinth_game.constants import ROOMS
 def describe_current_room(game_state):
   separator = ', '
   current_room = game_state['current_room'];
-  room = ROOMS[current_room]
+  room_data = ROOMS[current_room]
 
   print(f'== {current_room.upper()} ==')
 
-  print(room['description'])
+  print(room_data['description'])
 
-  if (room['items']):
-    print('Заметные предметы: ' + separator.join(room['items']) )
+  if room_data['items']:
+    print('Заметные предметы: ' + separator.join(room_data['items']))
   
-  print('Выходы: ' + separator.join([f'{exit}: {room['exits'][exit]}' for exit in room['exits'].keys()]))
+  print('Выходы: ' + separator.join([f'{exit}: {room_data['exits'][exit]}' for exit in room_data['exits'].keys()]))
 
-  if (room['puzzle']):
+  if room_data['puzzle']:
     print("Кажется, здесь есть загадка (используйте команду solve).")

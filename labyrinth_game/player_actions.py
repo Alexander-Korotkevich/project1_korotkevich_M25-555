8e1235d4eafb4b281a5a3ff049db126282e86887
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-09-24 20:27:02 +0300

    add take_item

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -1,8 +1,10 @@
-import labyrinth_game.utils
+from labyrinth_game.utils import describe_current_room
+from labyrinth_game.constants import ROOMS
+
 
 def show_inventory(game_state):
   inventory = game_state['player_inventory']
-  if (inventory):
+  if inventory:
     print(f'Инвентарь: {', '.join(inventory)}')
   else:
     print('Инвентарь пуст!')  
@@ -17,9 +19,22 @@ def get_input(prompt="> "):
 
 def move_player(game_state, direction):
   exit = game_state['exits'].get(direction)
-  if (exit):
+  if exit:
     game_state['current_room'] = exit
     game_state['steps_taken'] += 1
-    labyrinth_game.utils.describe_current_room(game_state)
+    describe_current_room(game_state)
   else:
     print("Нельзя пойти в этом направлении.")  
+
+def take_item(game_state, item_name):
+  current_room = game_state['current_room']
+  room_data = ROOMS[current_room]
+  items = room_data['items']
+
+  if item_name in items:
+    game_state['player_inventory'].append(item_name)
+    room_data['items'].remove(item_name)
+    print(f'Вы подняли: {item_name}.')
+  else:
+    print("Такого предмета здесь нет.")
+

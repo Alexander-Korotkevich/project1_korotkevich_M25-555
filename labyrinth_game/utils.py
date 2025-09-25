dcf3ec86316abcad9e70894183e2eb1d4ddfbee7
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-09-25 15:31:13 +0300

    Update utils
    - add get_room_data,
    - add solve_puzzle

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -1,9 +1,17 @@
 from labyrinth_game.constants import ROOMS
+from labyrinth_game.player_actions import get_input
+
+def get_room_data(game_state):
+  # Возвращает название текущей комнаты и ее данные
+
+  current_room = game_state['current_room'];
+  room_data = ROOMS.get(current_room)
+
+  return [current_room, room_data]
 
 def describe_current_room(game_state):
   separator = ', '
-  current_room = game_state['current_room'];
-  room_data = ROOMS[current_room]
+  [current_room, room_data] = get_room_data(game_state)
 
   print(f'== {current_room.upper()} ==')
 
@@ -16,3 +24,26 @@ def describe_current_room(game_state):
 
   if room_data['puzzle']:
     print("Кажется, здесь есть загадка (используйте команду solve).")
+
+def solve_puzzle(game_state):
+  [current_room, room_data] = get_room_data(game_state)
+  puzzle = room_data['puzzle']
+
+  if not puzzle:
+    print("Загадок здесь нет.")
+    return
+
+  question = puzzle[0]
+  answer = puzzle[1]
+
+  # Выводит загадку
+  print(question)
+
+  input = get_input("Ваш ответ: ")
+
+  if input == answer:
+    print("Правильно! Даже древние хранители были бы впечатлены.")
+    ROOMS[current_room]['puzzle'] = None
+    #TODO: добавить награду
+  else:
+    print("Неверно. Попробуйте снова.")
